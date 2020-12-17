1cf7216dd946e1edf037d315b054efbfb966d653
meavv <c.c.c.p@bk.ru>
2020-12-17 22:44:05 +0500

    3.3. Разбор первой программы. [#431425]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Evgeniy");
+        System.out.println("26.12.1994");
+    }
+}
