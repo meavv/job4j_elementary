a867e352eb6374b5df45621261e135c033c7b140
meavv <c.c.c.p@bk.ru>
2021-02-15 23:23:56 +0500

    Задача

---
+++ src/ru/job4j/condition/EvenCheck.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+import java.util.Arrays;
+
+public class EvenCheck {
+    public static boolean check(int num) {
+        return num % 2 == 0;
+
+    }
+
+    public static void main(String[] args) {
+        int num = 2;
+
+        System.out.println( EvenCheck.check(num));
+    }
+
+}
+
