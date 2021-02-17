b3e8a60c108301f526ee480696c2b5df82d3c399
e.malgin <4523949424qQ@>
2021-02-17 19:04:51 +0500

    0. Что такое тестирование [#431437]

---
+++ test/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.converter;
+import org.junit.Test;
+
+public class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+    }
+
+    @Test
+    public void rubleToDollar() {
+    }
+}
