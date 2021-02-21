77419e0e5f2e0aa44c5f229a3565293e11c07f13
meavv <c.c.c.p@bk.ru>
2021-02-22 00:09:25 +0500

    4.6.1. Switch [#431361]

---
+++ test/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwitchWeekTest {
+
+    @Test
+    public void nameOfDay1() {
+        String expected = "Понедельник";
+        String out = SwitchWeek.nameOfDay(1);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void nameOfDay4() {
+        String expected = "Четверг";
+        String out = SwitchWeek.nameOfDay(4);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void nameOfDay7() {
+        String expected = "Воскресенье";
+        String out = SwitchWeek.nameOfDay(7);
+        Assert.assertEquals(expected, out);
+    }
+    @Test
+    public void nameOfDay10() {
+        String expected = "Ошибка";
+        String out = SwitchWeek.nameOfDay(10);
+        Assert.assertEquals(expected, out);
+    }
+}
