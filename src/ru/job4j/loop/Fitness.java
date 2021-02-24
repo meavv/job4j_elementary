094e1a44e87e73e9f72f6236fd1589a2d49a5f43
e.malgin <4523949424qQ@>
2021-02-24 14:56:02 +0500

    5.4. Протеиновая диета [#431462]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan <= nik) {
+            nik *= 2;
+            ivan *= 3;
+            month += 1;
+            System.out.println(month);
+
+
+        }
+        return month;
+    }
+}
