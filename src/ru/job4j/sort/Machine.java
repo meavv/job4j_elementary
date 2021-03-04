c98f6a4686c0d366643ecc3e013cb64d8e95ed91
meavv <c.c.c.p@bk.ru>
2021-03-04 12:32:33 +0500

    6.9. Жадный алгоритм. Сдача в кофе машине [#431373]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -11,7 +11,7 @@ public class Machine {
         for (int i = 0; i < coins.length; i++) {
             while ((sdacha - coins[i]) >= 0) {
                 sdacha = sdacha - coins[i];
-                rsl[i] = coins[i];
+                rsl[size] = coins[i];
                 size = i + 1;
             }
         }
